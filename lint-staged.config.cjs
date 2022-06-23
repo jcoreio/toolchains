09156cf6a48b778cc7ae7c7094497a85e0546887
Andy Edwards <jedwards@fastmail.com>
2022-06-23 14:23:24 -0500

    chore: fix repo's own lint-staged config

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,4 +1,13 @@
 /* eslint-env node */
 module.exports = {
-  ...require('@jcoreio/toolchain/lint-staged.config.cjs'),
+  ...Object.fromEntries(
+    [
+      ...Object.entries(require('@jcoreio/toolchain/lint-staged.config.cjs')),
+    ].map(([key, value]) => [`./{packages,test}/${key}`, value])
+  ),
+  ...Object.fromEntries(
+    [
+      ...Object.entries(require('@jcoreio/toolchain/lint-staged.config.cjs')),
+    ].map(([key, value]) => [`./${key}`, value])
+  ),
 }
