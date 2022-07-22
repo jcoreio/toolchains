b887c4dfddc3172c4c2c717d46c42551059ccec6
Andy Edwards <jedwards@fastmail.com>
2022-07-22 12:13:36 -0500

    fix: error in test/coverage if no test toolchain is installed

--- packages/base/scripts/coverage.cjs
+++ packages/base/scripts/coverage.cjs
@@ -1,6 +1,12 @@
+const getPlugins = require('../util/getPlugins.cjs')
 const getPluginsAsyncFunction = require('../util/getPluginsAsyncFunction.cjs')
 
 exports.run = async function (args = []) {
+  if (!getPlugins('coverage').length) {
+    throw new Error(
+      'missing test toolchain, install @jcoreio/toolchain-mocha (there may be alternatives in the future)'
+    )
+  }
   await getPluginsAsyncFunction('coverage')(args)
 }
 exports.description = 'run tests with code coverage'
--- packages/base/scripts/test.cjs
+++ packages/base/scripts/test.cjs
@@ -1,6 +1,12 @@
+const getPlugins = require('../util/getPlugins.cjs')
 const getPluginsAsyncFunction = require('../util/getPluginsAsyncFunction.cjs')
 
 exports.run = async function (args = []) {
+  if (!getPlugins('test').length) {
+    throw new Error(
+      'missing test toolchain, install @jcoreio/toolchain-mocha (there may be alternatives in the future)'
+    )
+  }
   await getPluginsAsyncFunction('test')(args)
 }
 exports.description = 'run tests'
