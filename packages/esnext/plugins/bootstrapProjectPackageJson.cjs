a92672b4dbb76930fad770835651d1ea0f17b829
Andy Edwards <jedwards@fastmail.com>
2023-06-13 22:53:31 -0400

    chore: fix deps and ci config

--- packages/esnext/plugins/bootstrapProjectPackageJson.cjs
+++ packages/esnext/plugins/bootstrapProjectPackageJson.cjs
@@ -1,5 +1,5 @@
 module.exports = [
-  packageJson => {
+  (packageJson) => {
     const { dependencies } = require('../package.json')
     if (!packageJson.dependencies) packageJson.dependencies = {}
     packageJson.dependencies['@babel/runtime'] = dependencies['@babel/runtime']
