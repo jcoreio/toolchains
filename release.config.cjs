43bcc4e5c4e40bd1e2b853bc1c22cb3cee0bb146
Andy Edwards <jedwards@fastmail.com>
2023-06-15 00:16:58 -0400

    ci: set up semantic-release

---
+++ release.config.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node, es2018 */
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/exec',
+      {
+        publishCmd:
+          'pnpm -r exec npm version ${nextRelease.version} && pnpm publish -r',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+  branches: ['main', { name: 'beta', prerelease: true }],
+}
