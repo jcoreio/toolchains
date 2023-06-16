5091d06dd5e9fad61e13f36be4d2bc9155f21c67
Andy Edwards <jedwards@fastmail.com>
2023-06-16 17:33:17 -0400

    fix: chmod githooks to 755 on installation

--- packages/base/scripts/bootstrap/installGitHooks.cjs
+++ packages/base/scripts/bootstrap/installGitHooks.cjs
@@ -4,6 +4,9 @@ const execa = require('../../util/execa.cjs')
 const Path = require('path')
 const dedent = require('dedent-js')
 const { findGitDir } = require('../../util/findUps.cjs')
+const fs = require('fs-extra')
+
+const githooksDir = Path.resolve(__dirname, '..', '..', 'githooks')
 
 async function installGitHooks() {
   const gitDir = findGitDir()
@@ -15,13 +18,16 @@ async function installGitHooks() {
       after you run \`git init\`, try \`pnpm exec install-git-hooks\`.
     `)
   } else {
+    // chmod in case pnpm doesn't preserve mode of hooks scripts
+    await Promise.all(
+      (
+        await fs.readdir(githooksDir)
+      ).map((hook) => fs.chmod(Path.join(githooksDir, hook), 0o755))
+    )
     await execa('git', [
       'config',
       'core.hooksPath',
-      Path.relative(
-        Path.dirname(gitDir),
-        Path.resolve(__dirname, '..', '..', 'githooks')
-      ),
+      Path.relative(Path.dirname(gitDir), githooksDir),
     ])
     // eslint-disable-next-line no-console
     console.log('successfully installed git hooks!')
