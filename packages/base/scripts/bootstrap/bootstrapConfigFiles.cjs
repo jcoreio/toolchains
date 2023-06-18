118dff9f17a66cc5a09d3a125e4321d66cecd432
Andy Edwards <jedwards@fastmail.com>
2023-06-18 00:20:10 -0500

    fix: avoid overwriting already bootstrapped config files

--- packages/base/scripts/bootstrap/bootstrapConfigFiles.cjs
+++ packages/base/scripts/bootstrap/bootstrapConfigFiles.cjs
@@ -1,5 +1,6 @@
 const fs = require('../../util/projectFs.cjs')
 const getPluginsObject = require('../../util/getPluginsObject.cjs')
+const { name } = require('../../package.json')
 
 async function bootstrapConfigFiles() {
   const files = await getPluginsObject('getConfigFiles')
@@ -7,7 +8,12 @@ async function bootstrapConfigFiles() {
     const value = files[file]
     const content = typeof value === 'string' ? value : value.content
     const overwrite = typeof value === 'string' ? false : value.overwrite
-    if (overwrite || !(await fs.pathExists(file))) {
+    if (
+      overwrite === true ||
+      !(await fs.pathExists(file)) ||
+      (content.includes(name) &&
+        !(await fs.readFile(file, 'utf8')).includes(name))
+    ) {
       await fs.writeFile(file, content, 'utf8')
       // eslint-disable-next-line no-console
       console.error(`wrote ${file}`)
