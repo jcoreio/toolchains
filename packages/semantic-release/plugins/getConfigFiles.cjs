55d373250efd91e7d78393085dcb5e646d269a3c
Andy Edwards <jedwards@fastmail.com>
2023-07-02 21:12:47 -0500

    fix: use release.config.cjs, not .js

--- packages/semantic-release/plugins/getConfigFiles.cjs
+++ packages/semantic-release/plugins/getConfigFiles.cjs
@@ -4,7 +4,7 @@ const dedent = require('dedent-js')
 module.exports = [
   async function getConfigFiles() {
     return {
-      'release.config.js': dedent`
+      'release.config.cjs': dedent`
         /* eslint-env node, es2018 */
         module.exports = {
           extends: [require.resolve('${name}/release.config.cjs')],
