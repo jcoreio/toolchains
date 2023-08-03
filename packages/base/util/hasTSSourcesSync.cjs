48631e166452cfd2376c09b9056e962cb60c622a
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2023-08-03 17:47:23 -0500

    fix: recursively look for TS sources

--- packages/base/util/hasTSSourcesSync.cjs
+++ packages/base/util/hasTSSourcesSync.cjs
@@ -1,7 +1,24 @@
+const path = require('path')
 const fs = require('./projectFs.cjs')
 const once = require('./once.cjs')
 
-module.exports = once(function hasTSSourcesSync() {
-  const files = fs.readdirSync('src')
-  return files.some((f) => /\.[cm]?tsx?$/.test(f) && !/\.d\.[cm]?tsx?$/.test(f))
-})
+function hasTSSources(path, depth) {
+  if (depth > 10) return false
+  const files = fs.readdirSync(path, { withFileTypes: true })
+  // first check files in this directory
+  if (
+    files.some(
+      (f) =>
+        f.isFile() &&
+        /\.[cm]?tsx?$/.test(f.name) &&
+        !/\.d\.[cm]?tsx?$/.test(f.name)
+    )
+  )
+    return true
+  // if there were no TS files in this directory, recursively check subdirectories
+  return files.some(
+    (f) => f.isDirectory() && hasTSSources(path.join(path, f.name), depth + 1)
+  )
+}
+
+module.exports = once(() => hasTSSources('src', 0))
