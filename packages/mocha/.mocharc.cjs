f73682584692491c365db4f5dadfa9907f4fe020
Andy Edwards <jedwards@fastmail.com>
2023-10-15 10:55:38 -0500

    fix(mocha): if spec files are passed on cli, only run those files

--- packages/mocha/.mocharc.cjs
+++ packages/mocha/.mocharc.cjs
@@ -1,10 +1,17 @@
 const getPluginsArraySync = require('@jcoreio/toolchain/util/getPluginsArraySync.cjs')
+const extensions = getPluginsArraySync('sourceExtensions')
+const path = require('path')
+
+const files = process.argv.slice(2).filter((f) => {
+  const ext = path.extname(f)
+  return ext && extensions.includes(ext.substring(1))
+})
 
 module.exports = {
   require: [require.resolve('./util/configureMocha.cjs')],
   reporter: 'spec',
   spec: [
     require.resolve('./util/mochaWatchClearConsole.cjs'),
-    ...getPluginsArraySync('mochaSpecs'),
+    ...(files.length ? files : getPluginsArraySync('mochaSpecs')),
   ],
 }
