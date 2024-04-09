ce0c90f833d9d9ded5aab052f0e6406ed7bc95e5
Andy Edwards <jedwards@fastmail.com>
2024-04-08 21:59:44 -0500

    fix(@jcoreio/toolchain-mocha): set chai.config.includeStack = true

---
+++ packages/mocha/plugins/configureMocha.cjs
@@ -0,0 +1,20 @@
+const {
+  projectDir,
+  packageJson,
+} = require('@jcoreio/toolchain/util/findUps.cjs')
+const semver = require('semver')
+
+module.exports = [
+  () => {
+    const { dependencies = {}, devDependencies = {} } = packageJson
+    if (
+      semver.satisifies(
+        '4.9999.9999',
+        dependencies.chai || devDependencies.chai
+      )
+    ) {
+      const chai = require(require.resolve('chai', { paths: [projectDir] }))
+      chai.config.includeStack = true
+    }
+  },
+]
