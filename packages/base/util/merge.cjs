9339366d2461880f1a5fd62d67c9e97eaf15cbae
Andy Edwards <jedwards@fastmail.com>
2024-04-29 14:09:57 -0500

    chore: fix issues with lodash replacements, update snapshots

--- packages/base/util/merge.cjs
+++ packages/base/util/merge.cjs
@@ -1,13 +1,15 @@
-module.exports = function merge(a, b) {
-  if (!(a instanceof Object) || !(b instanceof Object)) return a
-  for (const key in b) {
-    const aValue = a[key]
-    const bValue = b[key]
-    if (aValue instanceof Object && bValue instanceof Object) {
-      merge(aValue, bValue)
-    } else {
-      a[key] = bValue
+module.exports = function merge(a, ...b) {
+  return b.reduce((a, b) => {
+    if (!(a instanceof Object) || !(b instanceof Object)) return a
+    for (const key in b) {
+      const aValue = a[key]
+      const bValue = b[key]
+      if (aValue instanceof Object && bValue instanceof Object) {
+        merge(aValue, bValue)
+      } else {
+        a[key] = bValue
+      }
     }
-  }
-  return a
+    return a
+  }, a)
 }
