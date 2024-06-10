8dd8fdfd5d5303de53af1c8a2c1ab0924f3758b5
Andy Edwards <jedwards@fastmail.com>
2024-06-09 22:51:16 -0500

    test: get monorepo semantic-release test working

--- fixtures/monorepo/input/packages/foo/release.config.cjs
+++ fixtures/monorepo/input/packages/foo/release.config.cjs
@@ -2,13 +2,5 @@
 const base = require('@jcoreio/toolchain-semantic-release/release.config.cjs')
 module.exports = {
   ...base,
-  branches: [
-    '+([0-9])?(.{+([0-9]),x}).x',
-    'master',
-    'next',
-    'next-major',
-    { name: 'beta', prerelease: true },
-    { name: 'alpha', prerelease: true },
-  ],
   plugins: base.plugins.filter(p => /semantic-release\/(commit-analyzer|release-notes-generator)/.test(Array.isArray(p) ? p[0] : p))
 }
--- test/integration/monorepoSemanticReleaseTests.js
+++ test/integration/monorepoSemanticReleaseTests.js
@@ -1,4 +1,6 @@
 const { it } = require('mocha')
+const { expect } = require('chai')
+const path = require('path')
 const execa = require('execa')
 const fs = require('fs-extra')
 const banner = require('../util/banner')
@@ -14,10 +16,83 @@ it(`monorepo semantic-release`, async function () {
     stdio: 'inherit',
     env: { ...process.env, JCOREIO_TOOLCHAIN_SELF_TEST: '1' },
   }
-  await execa('pnpm', ['i'], execaOpts)
-  // await execa('pnpm', ['tc', 'prepublish'], execaOpts)
+  await execa('pnpm', ['i', '--prefer-offline'], execaOpts)
   await execa('git', ['init'], execaOpts)
+  await execa(
+    'git',
+    [
+      'remote',
+      'add',
+      'origin',
+      'https://github.com/jcoreio/toolchain-monorepo-test.git',
+    ],
+    execaOpts
+  )
+  await execa('git', ['config', 'push.autoSetupRemote', 'true'], execaOpts)
   await execa('git', ['add', '.'], execaOpts)
-  await execa('git', ['commit', '-m', 'feat: initial commit'], execaOpts)
-  await execa('pnpm', ['-r', 'run', 'tc', 'release'], execaOpts)
+  await execa(
+    'git',
+    [
+      'commit',
+      '-m',
+      'feat(@jcoreio/toolchain-monorepo-test-foo): initial commit',
+    ],
+    execaOpts
+  )
+  await execa('git', ['push', '--force', 'origin', 'master'], execaOpts)
+
+  function execaPipe(command, args, options) {
+    const child = execa('pnpm', ['tc', 'release'], {
+      ...options,
+      stdio: 'pipe',
+      encoding: 'utf8',
+      maxBuffer: 30 * 1024 * 1024,
+    })
+    child.stdout.pipe(process.stdout)
+    child.stderr.pipe(process.stderr)
+    return child
+  }
+
+  {
+    const { stdout } = await execaPipe('pnpm', ['tc', 'release'], {
+      ...execaOpts,
+      cwd: path.join(execaOpts.cwd, 'packages', 'foo'),
+    })
+    expect(stdout).to.contain(
+      'Skip @jcoreio/toolchain-monorepo-test-foo-v1.0.0 tag creation'
+    )
+    expect(stdout).to.contain(
+      '* @jcoreio/toolchain-monorepo-test-foo: initial commit'
+    )
+  }
+  {
+    const { stdout } = await execaPipe('pnpm', ['tc', 'release'], {
+      ...execaOpts,
+      cwd: path.join(execaOpts.cwd, 'packages', 'bar'),
+    })
+    expect(stdout).not.to.contain(
+      'Skip @jcoreio/toolchain-monorepo-test-bar-v1.0.0 tag creation'
+    )
+    expect(stdout).to.contain('There are no relevant changes')
+  }
+
+  await execa(
+    'git',
+    ['commit', '-a', '-m', 'feat: new initial commit'],
+    execaOpts
+  )
+  await execa('git', ['push', '--force', 'origin', 'master'], execaOpts)
+
+  {
+    for (const pkg of ['foo', 'bar']) {
+      const { stdout } = await execaPipe('pnpm', ['tc', 'release'], {
+        ...execaOpts,
+        cwd: path.join(execaOpts.cwd, 'packages', pkg),
+      })
+      expect(stdout).to.contain(
+        `Skip @jcoreio/toolchain-monorepo-test-${pkg}-v1.0.0 tag creation`
+      )
+      expect(stdout).to.contain('* new initial commit')
+    }
+  }
 })
