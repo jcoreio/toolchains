00ce26d27aacf69dbff2385b6587608fcf07aff3
Andy Edwards <jedwards@fastmail.com>
2024-06-10 07:44:00 -0500

    chore: try to fix ci build

--- fixtures/monorepo/input/packages/bar/release.config.cjs
+++ fixtures/monorepo/input/packages/bar/release.config.cjs
@@ -2,5 +2,9 @@
 const base = require('@jcoreio/toolchain-semantic-release/release.config.cjs')
 module.exports = {
   ...base,
-  plugins: base.plugins.filter(p => /semantic-release\/(commit-analyzer|release-notes-generator)/.test(Array.isArray(p) ? p[0] : p))
+  plugins: base.plugins.filter((p) =>
+    /semantic-release\/(commit-analyzer|release-notes-generator)/.test(
+      Array.isArray(p) ? p[0] : p
+    )
+  ),
 }
