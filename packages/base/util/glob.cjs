c56e54b9071c2d499acfd1140da50e04fa1dfefe
Andy Edwards <jedwards@fastmail.com>
2025-04-22 20:07:56 -0500

    fix: ignore node_modules in all glob operations

--- packages/base/util/glob.cjs
+++ packages/base/util/glob.cjs
@@ -1,13 +1,18 @@
 const { glob, globSync, globIterate, globIterateSync } = require('glob')
 const { projectDir } = require('./findUps.cjs')
 
+const extendOptions = (options) => ({
+  cwd: projectDir,
+  ...options,
+  ignore: [...(options.ignore || []), '**/node_modules/**'],
+})
+
 module.exports = {
-  glob: (what, options) => glob(what, { cwd: projectDir, ...options }),
-  globSync: (what, options) => globSync(what, { cwd: projectDir, ...options }),
-  globIterate: (what, options) =>
-    globIterate(what, { cwd: projectDir, ...options }),
+  glob: (what, options) => glob(what, extendOptions(options)),
+  globSync: (what, options) => globSync(what, extendOptions(options)),
+  globIterate: (what, options) => globIterate(what, extendOptions(options)),
   globIterateSync: (what, options) =>
-    globIterateSync(what, { cwd: projectDir, ...options }),
+    globIterateSync(what, extendOptions(options)),
   globExists: async (what, options) => {
     for await (const _ of module.exports.globIterate(what, options)) {
       return true
