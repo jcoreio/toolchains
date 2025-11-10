6468c340decab650911b8d93753b3caaf42ce70b
Andy Edwards <jedwards@fastmail.com>
2025-11-10 17:12:04 -0600

    feat: convert src .ts file paths in bin etc to dist .js/.mjs
    this is WIP, need to convert .ts extensions in requires and imports

--- packages/base/plugins/buildDistPackageJson.cjs
+++ packages/base/plugins/buildDistPackageJson.cjs
@@ -1,4 +1,6 @@
 const mapValues = require('../util/mapValues.cjs')
+const { toolchainConfig } = require('@jcoreio/toolchain/util/findUps.cjs')
+const { outputEsm } = toolchainConfig
 
 module.exports = [
   async function buildDistPackageJson(packageJson) {
@@ -6,16 +8,32 @@ module.exports = [
     delete packageJson.scripts
     delete packageJson.config
 
-    function replaceDist(path) {
-      return path.replace(/^(\.\/)?dist\//, '$1')
+    function replaceDist(path, ext) {
+      path = path.replace(/^(\.\/)?(src|dist)\//, '$1')
+      if (ext) path = path.replace(/\.tsx?$/, ext)
+      return path
     }
 
     for (const key of ['main', 'module', 'browser', 'types', 'bin']) {
       if (typeof packageJson[key] === 'string') {
-        packageJson[key] = replaceDist(packageJson[key])
+        packageJson[key] = replaceDist(
+          packageJson[key],
+          key === 'module' ? '.mjs'
+          : key === 'types' ? undefined
+          : outputEsm ? '.mjs'
+          : '.js'
+        )
       }
     }
-    for (const key of ['bin', 'directories']) {
+    for (const key of ['bin']) {
+      if (typeof packageJson[key] === 'object' && packageJson[key] != null) {
+        packageJson[key] = mapValues(packageJson[key], (path) =>
+          replaceDist(path, outputEsm ? '.mjs' : '.js')
+        )
+      }
+    }
+
+    for (const key of ['directories']) {
       if (typeof packageJson[key] === 'object' && packageJson[key] != null) {
         packageJson[key] = mapValues(packageJson[key], replaceDist)
       }
