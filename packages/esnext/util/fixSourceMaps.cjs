cc6cddb370a37b894af3f77ab89132a7e3213f3d
Andy Edwards <jedwards@fastmail.com>
2025-11-25 22:44:27 -0600

    fix: remove sourcesContent from output souces maps
    fix #39

--- packages/esnext/util/fixSourceMaps.cjs
+++ packages/esnext/util/fixSourceMaps.cjs
@@ -9,12 +9,16 @@ module.exports = async function fixSourceMaps() {
   await Promise.all(
     mapFiles.map(async (file) => {
       const content = await fs.readJson(file)
-      const { sources } = content
+      const { sources, sourcesContent } = content
       let changed = false
       for (let i = 0; i < sources.length; i++) {
         if (sources[i].startsWith('../src/')) {
           changed = true
           sources[i] = sources[i].substring('../'.length)
+        }
+        if (sourcesContent && sourcesContent[i] != null) {
+          changed = true
+          sourcesContent[i] = null
         }
       }
       if (changed) await fs.writeJson(file, content)
