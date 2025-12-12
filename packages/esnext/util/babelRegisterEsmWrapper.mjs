db6231a6fe4b1e919f1ac130385b28eeca69e809
Andy Edwards <jedwards@fastmail.com>
2025-12-12 11:19:06 -0600

    fix(@jcoreio/toolchain-esnext): fix mocha CJS resolution issues
    In a project with `outputEsm: false`, mocha's `importOrRequire` can end up trying to import
    the file via `babelRegisterEsmWrapper` ^Cd failing with a "require is not defined in ES"
    error that it mocha doesn't identify as a reason to try `require`ing the file instead.

--- packages/esnext/util/babelRegisterEsmWrapper.mjs
+++ packages/esnext/util/babelRegisterEsmWrapper.mjs
@@ -3,36 +3,73 @@ import { fileURLToPath, pathToFileURL } from 'url'
 import { resolve as baseResolve, load as wrappedLoad } from 'babel-register-esm'
 import { projectDir } from '@jcoreio/toolchain/util/findUps.cjs'
 import resolveAltType from './resolveAltType.cjs'
+import findUp from 'find-up'
+import fs from 'fs-extra'
 
 const projectDirPrefix = pathToFileURL(projectDir) + '/'
 
+const packageJsonCache = new Map()
+let packageJsonCacheTime = Date.now()
+
+async function getPackageJson(cwd) {
+  const now = Date.now()
+  if (now - packageJsonCacheTime > 10000) {
+    packageJsonCache.clear()
+    packageJsonCacheTime = now
+  }
+  if (packageJsonCache.has(cwd)) return packageJsonCache.get(cwd)
+  const packageJsonFile = await findUp('package.json', { cwd })
+  const packageJson =
+    packageJsonFile ?
+      await fs.readJson(packageJsonFile).catch(() => undefined)
+    : undefined
+  packageJsonCache.set(cwd, packageJson)
+  return packageJson
+}
+
 export async function resolve(specifier, context, nextResolve) {
-  if (
-    specifier.startsWith(projectDirPrefix) ||
-    (specifier.startsWith('.') &&
-      context.parentURL.startsWith('file:') &&
-      !context.parentURL.includes('/node_modules/'))
-  ) {
-    const basedir =
-      context.parentURL ?
-        path.dirname(fileURLToPath(context.parentURL))
-      : process.cwd()
-    if (!specifier.startsWith('.')) {
-      specifier = path.relative(basedir, fileURLToPath(specifier))
-      if (!specifier.startsWith('.')) specifier = `./${specifier}`
-    }
-    const altTypeResolved = resolveAltType(specifier, basedir)
-    if (altTypeResolved) {
-      return {
-        url: pathToFileURL(path.resolve(basedir, altTypeResolved)).toString(),
-        shortCircuit: true,
-        format:
-          process.env.JCOREIO_TOOLCHAIN_CJS ? 'commonjs'
-          : process.env.JCOREIO_TOOLCHAIN_ESM ? 'module'
-          : context.format,
+  if (specifier.startsWith('file:') || specifier.startsWith('.')) {
+    const resolved = new URL(specifier, context.parentURL).toString()
+    if (
+      resolved.startsWith(projectDirPrefix) &&
+      !resolved.includes('/node_modules/')
+    ) {
+      const basedir =
+        context.parentURL ?
+          path.dirname(fileURLToPath(context.parentURL))
+        : process.cwd()
+      const file = fileURLToPath(resolved)
+      const altTypeResolved = resolveAltType(
+        path.relative(basedir, file),
+        basedir
+      )
+      if (altTypeResolved) {
+        return {
+          url: resolved,
+          shortCircuit: true,
+          format:
+            process.env.JCOREIO_TOOLCHAIN_CJS ? 'commonjs'
+            : process.env.JCOREIO_TOOLCHAIN_ESM ? 'module'
+            : context.format,
+        }
+      }
+      if (
+        /\.[jt]sx?$/.test(resolved) &&
+        !process.env.JCOREIO_TOOLCHAIN_CJS &&
+        !process.env.JCOREIO_TOOLCHAIN_ESM
+      ) {
+        const packageJson = await getPackageJson(path.dirname(file))
+        if (packageJson) {
+          return {
+            url: resolved,
+            shortCircuit: true,
+            format: packageJson.type || 'commonjs',
+          }
+        }
       }
     }
   }
+
   return await baseResolve(specifier, context, nextResolve)
 }
 
