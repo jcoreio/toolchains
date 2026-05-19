5e633dcd4dc9ef954a785d23eed0e39785144350
Andy Edwards <jedwards@fastmail.com>
2026-05-19 17:38:01 -0500

    fix(@jcoreio/toolchain-typescript): fix more issues with calling @arethetypeswrong/cli

--- packages/typescript/plugins/smokeTestBuild.cjs
+++ packages/typescript/plugins/smokeTestBuild.cjs
@@ -1,15 +1,27 @@
 const execa = require('@jcoreio/toolchain/util/execa.cjs')
 const { toolchainConfig } = require('@jcoreio/toolchain/util/findUps.cjs')
+const fs = require('fs/promises')
+const path = require('path')
 
 module.exports = [
   async function smokeTestBuild() {
+    const attwDir = path.resolve(
+      __dirname,
+      '../node_modules/@arethetypeswrong/cli'
+    )
+    const { bin } = JSON.parse(
+      await fs.readFile(path.resolve(attwDir, 'package.json'))
+    )
+    const attw = path.resolve(
+      attwDir,
+      typeof bin === 'string' ? bin : bin && bin.attw
+    )
     await execa(
-      'pnpm',
+      process.execPath,
       [
-        'exec',
         // used to just dlx the package but we need to pin transitive deps to work
         // around bugs
-        'attw',
+        attw,
         '--pack',
         '.',
         ...(toolchainConfig.outputCjs === false ?
