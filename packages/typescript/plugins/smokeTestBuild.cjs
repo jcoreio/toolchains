a3716149ff7ca3aa7b5ee06940a6381cbf8c813a
Andy Edwards <jedwards@fastmail.com>
2026-05-19 17:54:41 -0500

    fix(@jcoreio/toolchain-typescript): work around @arethetypeswrong/cli bs again

--- packages/typescript/plugins/smokeTestBuild.cjs
+++ packages/typescript/plugins/smokeTestBuild.cjs
@@ -1,34 +1,24 @@
 const execa = require('@jcoreio/toolchain/util/execa.cjs')
-const { toolchainConfig } = require('@jcoreio/toolchain/util/findUps.cjs')
-const fs = require('fs/promises')
 const path = require('path')
+const {
+  projectDir,
+  toolchainConfig,
+} = require('@jcoreio/toolchain/util/findUps.cjs')
 
 module.exports = [
   async function smokeTestBuild() {
-    const attwDir = path.resolve(
-      __dirname,
-      '../node_modules/@arethetypeswrong/cli'
-    )
-    const { bin } = JSON.parse(
-      await fs.readFile(path.resolve(attwDir, 'package.json'))
-    )
-    const attw = path.resolve(
-      attwDir,
-      typeof bin === 'string' ? bin : bin && bin.attw
-    )
     await execa(
-      process.execPath,
+      'pnpm',
       [
-        // used to just dlx the package but we need to pin transitive deps to work
-        // around bugs
-        attw,
+        'exec',
+        'attw',
         '--pack',
-        '.',
+        path.join(projectDir, 'dist'),
         ...(toolchainConfig.outputCjs === false ?
           ['--profile', 'esm-only']
         : []),
       ],
-      { cwd: 'dist' }
+      { cwd: __dirname }
     )
   },
 ]
